99d2aba699856d0d9b23ea2fd55ba3eb47b4709d
dmitri9149 <dmitri.as.name@gmail.com>
2020-11-14 10:14:40 +0200

    components are added

--- courseinfo/src/index.js
+++ courseinfo/src/index.js
@@ -1,6 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = (props) => {
+  return (
+    <div>
+      <p>
+        {props.course}
+      </p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      {props.part}
+    </div>
+  )	  
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      {props.exercises}
+    </div>
+  )	  
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -11,8 +37,10 @@ const App = () => {
   const exercises3 = 14
 
   return (
-    <div>
-      <h1>{course}</h1>
+    <>
+      <Header course={course} />
+      <Content part={part1} />
+      <Total exercises={exercises1} />
       <p>
         {part1} {exercises1}
       </p>
@@ -23,7 +51,7 @@ const App = () => {
         {part3} {exercises3}
       </p>
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    </div>
+    </>
   )
 }
 
