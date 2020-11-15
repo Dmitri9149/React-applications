c357cc1b91e3d087bf24f0225e32b2067436db51
dmitri9149 <dmitri.as.name@gmail.com>
2020-11-15 22:01:42 +0200

    extra display fields

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -29,6 +29,9 @@ const App = () => {
       <Display text={"good"} value={good} />
       <Display text={"neutral"} value={neutral} />
       <Display text={"bad"} value={bad} />
+      <Display text={"all"} value={good+bad+neutral} />
+      <Display text={"average"} value={(good-bad)/(good+bad+neutral)} />
+      <Display text={"positive"} value={(good)/(good+bad+neutral)*100+"%"} />
     </div>
   )
 }
