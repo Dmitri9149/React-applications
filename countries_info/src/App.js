611f4bb15e2327f6fe0156e27267b5c162e8d540
dmitri9149 <dmitri.as.name@gmail.com>
2020-11-27 20:55:37 +0200

    Country comp added

--- countries_info/src/App.js
+++ countries_info/src/App.js
@@ -1,7 +1,7 @@
 import React,  { useState, useEffect } from 'react';
 import axios from 'axios'
-{/*import Country from './components/Country'
-import Filter from './components/Filter' */}
+import Country from './components/Country'
+{/*import Filter from './components/Filter' */}
 
 const App = () => {
 
@@ -22,6 +22,7 @@ const App = () => {
 
   return (
     <>
+    <Country country = {country} />
     </>  
   )
 } 
