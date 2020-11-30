128e820cde5de849df2e868734c306d544241988
dmitri9149 <dmitri.as.name@gmail.com>
2020-11-30 20:07:17 +0200

    Country component

--- countries_info/src/App.js
+++ countries_info/src/App.js
@@ -1,13 +1,12 @@
 import React,  { useState, useEffect } from 'react';
 import axios from 'axios'
-{/*import Country from './components/Country' /*}
+import Country from './components/Country'
 {/*import Filter from './components/Filter' */}
 
 const App = () => {
 
   const [countries, setCountries] = useState([])
   const [filter, setFilter] = useState('')
-  const [weather, setWeather] = useState('')
   
 
   useEffect(() =>  {
@@ -20,21 +19,7 @@ const App = () => {
       })
   }, [])
 
-  const getWeather = (capital) =>  {
-    console.log('effect weather')
-    const url = new URL('http://api.weatherstack.com/current?access_key=""&query=Helsinki')
-{/*    const params = new URLSearchParams(url.search); */}
-    console.log('app key', process.env.REACT_APP_WEATHER)
-    url.searchParams.set('access_key', process.env.REACT_APP_WEATHER)
-    url.searchParams.set('query' , capital)
-    console.log('url', url)
-    axios
-      .get(url)
-      .then(response => {
-        console.log('promise weather fulfilled')
-        setWeather(response.data)
-      })
-  }
+
 
   const candidatesToShow = (filter === '')
   ? countries
@@ -51,17 +36,10 @@ const App = () => {
   )
   : candidatesToShow.length === 1 
   ? <div> 
-      <h2>{candidatesToShow[0].name}</h2>
-      <p>Capital: {'  '} {candidatesToShow[0].capital}</p>
-      <p>Population: {'  '} {candidatesToShow[0].population}</p>
-      <h3>Languages:</h3>
-      <ul>
-       {candidatesToShow[0].languages.map(lang => <li key={lang.name}>{lang.name}</li>)} 
-      </ul>
-      <img max-width = {250} height = {80} src={candidatesToShow[0].flag} alt="flag" />
-      <p>
-        Temperature feelslike:
-      </p>
+      <Country 
+        country = {candidatesToShow[0]}
+        filter = {filter}
+      />
     </div>
   : "Too many matches, use a more specific filter"
 
@@ -71,8 +49,6 @@ const App = () => {
     console.log(process.env.REACT_APP_WEATHER)
     console.log("singl")
     setFilter(country.name)
-    console.log("single weather")
-    getWeather(country.capital)
   }
 
   return (
@@ -80,11 +56,7 @@ const App = () => {
       <div> Find countries which include the subword:{' '} 
       <input val = {filter} onChange = {handleFilter} />
       </div>
-{/*      <ul>
-        {countriesToShow.map(x => <li>{x.name}</li>)}
-</ul>   */}
       {whatToShow}
-{/*    <Country countries = {countries} />  */}
     </>  
   )
 } 
--- countries_info/src/components/Country.js
+++ countries_info/src/components/Country.js
@@ -1,4 +1,60 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
+import axios from 'axios'
+
+const Country = ({filter, country}) => {
+
+    const [weatherCurrent, setWeatherCurrent]= useState({})
+    const [weatherCondition, setWeatherCondition]= useState({})
+
+    console.log("Country effect !!!!!!!!!!")
+    const url = new URL('http://api.weatherstack.com/current?access_key=""&query=Helsinki')
+    url.searchParams.set('access_key', process.env.REACT_APP_WEATHER)
+    url.searchParams.set('query' , country.capital)
+    console.log('filter', filter)
+    console.log('url', url)
+
+    useEffect(() =>  {
+        axios
+          .get(url)
+          .then(response => {
+            console.log('weather promise fulfilled')
+            setWeatherCurrent(response.data.current)
+            setWeatherCondition(response.data)
+          })
+      }, [])
+
+    return (
+        <>
+        <div>
+            <h2>{country.name}</h2>
+            <p>Capital: {'  '} {country.capital}</p>
+            <p>Population: {'  '} {country.population}</p>
+            <h3>Languages:</h3>
+            <ul>
+                {country.languages.map(lang => <li key={lang.name}>{lang.name}</li>)} 
+            </ul>
+            <img max-width = {250} height = {80} src={country.flag} alt="flag" />
+            <p>
+                Temperature feelslike: {weatherCurrent.feelslike}
+            </p>
+            <h2>Weather in {country.capital}</h2>
+            <p>
+                <b>
+                temperature :&nbsp; 
+                </b> 
+                {weatherCurrent.temperature} Celsius
+            </p>
+            <img max-width = {40} height = {40} src = {weatherCurrent.weather_icons} alt="Weather" /> 
+            <p>
+                <b>
+                wind :&nbsp; 
+                </b>   
+                {weatherCurrent.wind_speed} kph direction {weatherCurrent.wind_dir}
+            </p>
+        </div>
+        </>
+    )
+}
 
 
 export default Country
